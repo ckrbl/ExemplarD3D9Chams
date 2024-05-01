6de45430a6d764b0750772cfa8fcf7b83a597e3d
ckrbl <99300381+ckrbl@users.noreply.github.com>
2024-04-30 22:20:06 -0400

    Initialize D3D9 Device correctly using Imgui as a guide (#67)
    Initialize D3D9 Device correctly using imgui as a guide

--- src/D3DDeviceCreate.cpp
+++ src/D3DDeviceCreate.cpp
@@ -8,31 +8,18 @@ LRESULT CALLBACK MsgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
-bool GetD3D9Device(void** pTable, size_t tableSize)
+bool CreateDeviceD3DCopyTable(HWND hWnd, void** pTable, size_t tableSize)
 {
     bool bRet = false;
-    D3DPRESENT_PARAMETERS d3dpp = {0};
-    IDirect3DDevice9* pDummyDevice = NULL;
+    IDirect3DDevice9* pDummyDevice = nullptr;
     HRESULT hrCreateDevice;
-    HWND hWnd;
+    D3DPRESENT_PARAMETERS d3dpp = {0};
     IDirect3D9* pD3D;
-    WNDCLASSEX wc = {
-        sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, "DX", NULL};
-
-    (void)RegisterClassEx(&wc);
-    hWnd =
-        CreateWindow("DX", NULL, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, GetDesktopWindow(), NULL, wc.hInstance, NULL);
-    if (!hWnd)
-    {
-        MessageBox(nullptr, "CreateWindow failed", nullptr, MB_OK);
-        goto error_exit;
-    }
-
     pD3D = Direct3DCreate9(D3D_SDK_VERSION);
     if (!pD3D)
     {
         MessageBox(nullptr, "Direct3DCreate9 failed", nullptr, MB_OK);
-        goto cleanup_destroy_window;
+        goto exit;
     }
 
     d3dpp.Windowed = true;
@@ -53,8 +40,53 @@ bool GetD3D9Device(void** pTable, size_t tableSize)
     pDummyDevice->Release();
 cleanup_free_pd3d:
     pD3D->Release();
+exit:
+    return bRet;
+}
+
+bool GetD3D9Device(void** pTable, size_t tableSize)
+{
+    bool bRet = false;
+    HWND hWnd = NULL;
+    WNDCLASSEX wc = {sizeof(WNDCLASSEX),
+                     CS_CLASSDC,
+                     MsgProc,
+                     0L,
+                     0L,
+                     GetModuleHandle(nullptr),
+                     nullptr,
+                     nullptr,
+                     nullptr,
+                     nullptr,
+                     "DX",
+                     nullptr};
+
+    if (RegisterClassEx(&wc) == 0)
+    {
+        MessageBox(nullptr, "RegisterClassEx failed", nullptr, MB_OK);
+        goto exit;
+    }
+
+    hWnd = CreateWindow(wc.lpszClassName, nullptr, WS_OVERLAPPEDWINDOW, 100, 100, 300, 300, nullptr, nullptr,
+                        wc.hInstance, nullptr);
+    if (!hWnd)
+    {
+        MessageBox(nullptr, "CreateWindow failed", nullptr, MB_OK);
+        goto cleanup_register_class;
+    }
+
+    if (!CreateDeviceD3DCopyTable(hWnd, pTable, tableSize))
+    {
+        // Message already printed
+        goto cleanup_destroy_window;
+    }
+
+    bRet = true;
+
 cleanup_destroy_window:
     DestroyWindow(hWnd);
-error_exit:
+cleanup_register_class:
+    UnregisterClass(wc.lpszClassName, wc.hInstance);
+exit:
     return bRet;
 }
