93064835d008a22725976f1ecf39d6d14981581b
Chris K <99300381+ckrbl@users.noreply.github.com>
2024-05-02 19:45:39 -0400

    Cleanup replicated blocks with function macros

--- src/D3D9HookInit.cpp
+++ src/D3D9HookInit.cpp
@@ -3,6 +3,20 @@
 
 #pragma comment(lib, "detours.lib")
 
+#define STRINGIFY(x) #x
+
+#define CHECK_ERROR(FUNC, ERROR_TARGET)                                 \
+    if ((FUNC) != NO_ERROR)                                             \
+    {                                                                   \
+        MessageBox(nullptr, STRINGIFY(FUNC) " failed", nullptr, MB_OK); \
+        status = false;                                                 \
+        goto ERROR_TARGET;                                              \
+    }
+
+#define DETOUR_ATTACH(NAME, TABLE_IDX)          \
+    o##NAME = (t##NAME)(d3d9Device[TABLE_IDX]); \
+    CHECK_ERROR(DetourAttach(&(LPVOID&)o##NAME, x##NAME), decide);
+
 bool InitD3D9Hook(void* d3d9Device[D3D9_VTABLE_SIZE])
 {
 #define RESET_VTABLE_IDX 16
@@ -10,61 +24,37 @@ bool InitD3D9Hook(void* d3d9Device[D3D9_VTABLE_SIZE])
 #define DRAW_INDEXED_PRIMITIVE_VTABLE_IDX 82
 #define SET_STREAM_SOURCE_VTABLE_IDX 100
 
-#define DETOUR_ATTACH(NAME, TABLE_IDX)                              \
-    o##NAME = (t##NAME)(d3d9Device[TABLE_IDX]);                     \
-    if (DetourAttach(&(LPVOID&)o##NAME, x##NAME) != NO_ERROR)       \
-    {                                                               \
-        MessageBox(nullptr, "DetourAttach failed", nullptr, MB_OK); \
-        goto decide;                                                \
-    }
+    bool status;
 
-    bool status = false;
+    CHECK_ERROR(DetourTransactionBegin(), decide);
+    CHECK_ERROR(DetourUpdateThread(GetCurrentThread()), decide);
 
-    if (DetourTransactionBegin() != NO_ERROR)
-    {
-        MessageBox(nullptr, "DetourTransactionBegin failed", nullptr, MB_OK);
-        goto decide;
-    }
-
-    if (DetourUpdateThread(GetCurrentThread()) != NO_ERROR)
-    {
-        MessageBox(nullptr, "DetourTransactionBegin failed", nullptr, MB_OK);
-        goto decide;
-    }
-
-    DETOUR_ATTACH(Reset, RESET_VTABLE_IDX)
-    DETOUR_ATTACH(EndScene, ENDSCENE_VTABLE_IDX)
-    DETOUR_ATTACH(DrawIndexedPrimitive, DRAW_INDEXED_PRIMITIVE_VTABLE_IDX)
-    DETOUR_ATTACH(SetStreamSource, SET_STREAM_SOURCE_VTABLE_IDX)
+    DETOUR_ATTACH(Reset, RESET_VTABLE_IDX);
+    DETOUR_ATTACH(EndScene, ENDSCENE_VTABLE_IDX);
+    DETOUR_ATTACH(DrawIndexedPrimitive, DRAW_INDEXED_PRIMITIVE_VTABLE_IDX);
+    DETOUR_ATTACH(SetStreamSource, SET_STREAM_SOURCE_VTABLE_IDX);
 
     status = true;
 
 decide:
     if (status)
     {
-        if (DetourTransactionCommit() != NO_ERROR)
-        {
-            MessageBox(nullptr, "DetourTransactionCommit failed", nullptr, MB_OK);
-            status = false;
-            goto exit;
-        }
+        CHECK_ERROR(DetourTransactionCommit(), exit);
     }
     else
     {
-        if (DetourTransactionAbort() != NO_ERROR)
-        {
-            MessageBox(nullptr, "DetourTransactionAbort failed", nullptr, MB_OK);
-            goto exit;
-        }
+        CHECK_ERROR(DetourTransactionAbort(), exit);
     }
 
 exit:
     return status;
-
-#undef DETOUR_ATTACH
 
 #undef SET_STREAM_SOURCE_VTABLE_IDX
 #undef DRAW_INDEXED_PRIMITIVE_VTABLE_IDX
 #undef ENDSCENE_VTABLE_IDX
 #undef RESET_VTABLE_IDX
 }
+
+#undef DETOUR_ATTACH
+#undef CHECK_ERROR
+#undef STRINGIFY
