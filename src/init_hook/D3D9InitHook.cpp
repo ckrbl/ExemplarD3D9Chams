28b99f2db8e58499a77427b254ccc4b5a48a1b85
Chris K <99300381+ckrbl@users.noreply.github.com>
2024-05-07 18:51:38 -0400

    test

--- src/init_hook/D3D9InitHook.cpp
+++ src/init_hook/D3D9InitHook.cpp
@@ -166,7 +166,6 @@ int InitImgui(HWND window)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 
